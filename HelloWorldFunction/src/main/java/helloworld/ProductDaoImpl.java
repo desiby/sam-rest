ca21c2db0e55c6485bf03dd0081d04b735d0f151
Desire Bah Bioh <desire.bahbioh@stelligent.com>
2021-03-16 01:01:30 -0400

    add local env deployment

--- HelloWorldFunction/src/main/java/helloworld/ProductDaoImpl.java
+++ HelloWorldFunction/src/main/java/helloworld/ProductDaoImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -14,11 +15,18 @@ public class ProductDaoImpl implements ProductDao {
     private AmazonDynamoDB client;
 	private DynamoDBMapper mapper;
 
+    private static final boolean RUN_LOCALLY = false;
+    
     public ProductDaoImpl(){
-        //this.client = client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-        this.client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
-            new AwsClientBuilder.EndpointConfiguration("http://dynamodb:8000", "us-east-1"))
-            .build();
+        
+       if(RUN_LOCALLY){
+           this.client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+             new AwsClientBuilder.EndpointConfiguration("http://dynamodb:8000", "us-east-1"))
+              .build();
+       }else{
+          this.client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+       }
+        
         this.mapper = new DynamoDBMapper(this.client);
     }
     
